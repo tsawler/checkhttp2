1e2e775204576db107dad278469ca2514617985c
Trevor Sawler <trevor.sawler@gmail.com>
2018-03-08 19:38:32 -0400

    Formatting

--- checkHttp2.go
+++ checkHttp2.go
@@ -9,13 +9,11 @@ import (
 func main() {
 
 	url := "https://" + os.Args[1];
-
 	resp, err := http.Get(url)
 	if err != nil {
 		print(err.Error())
-
 	} else {
-		if (resp.StatusCode != 200) {
+		if resp.StatusCode != 200 {
 			fmt.Println("CRITICAL- " + os.Args[1] + " " + resp.Status)
 		} else {
 			fmt.Println("OK- " + os.Args[1] + " responded with " + resp.Status)
