4bdc53fdfcf6c7307f1edbe6a0a82b972780a1ab
Trevor Sawler <trevor.sawler@gmail.com>
2018-03-10 16:50:35 -0400

    Added test

---
+++ main_test.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"testing"
+	"checkhttp2/certificateutils"
+)
+
+func TestScanhost(t *testing.T) {
+	hostname := "www.google.com"
+	res, _ := certificateutils.GetCertificateDetails(hostname, 10)
+
+	if res.Hostname != "www.google.com:443" {
+		t.Error("Expected www.google.com:443, got ", res.Hostname)
+	}
+
+}
