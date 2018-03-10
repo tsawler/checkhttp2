5d7cd3428f24a1ec5a7f0856357a9caf025c1a27
Trevor Sawler <trevor.sawler@gmail.com>
2018-03-09 20:12:12 -0400

    Refactoring

--- main.go
+++ main.go
@@ -12,67 +12,11 @@ import (
 	"github.com/pkg/errors"
 	"time"
 	"strconv"
+	"github.com/tsawler/checkhttp2/messages"
 )
 
-type NagiosStatusVal int
 
-const (
-	NAGIOS_OK       NagiosStatusVal = iota
-	NAGIOS_WARNING
-	NAGIOS_CRITICAL
-	NAGIOS_UNKNOWN
-)
 
-var (
-	valMessages = []string{
-		"OK:",
-		"WARNING:",
-		"CRITICAL:",
-		"UNKNOWN:",
-	}
-)
-
-// Find the highest value and combine all the messages. Things win in the order of highest to lowest.
-type NagiosStatus struct {
-	Message string
-	Value   NagiosStatusVal
-}
-
-func (status *NagiosStatus) Aggregate(otherStatuses []*NagiosStatus) {
-	for _, s := range otherStatuses {
-		if status.Value < s.Value {
-			status.Value = s.Value
-		}
-
-		status.Message += " - " + s.Message
-	}
-}
-
-// Exit with an UNKNOWN status and appropriate message
-func Unknown(output string) {
-	ExitWithStatus(&NagiosStatus{output, NAGIOS_UNKNOWN})
-}
-
-// Exit with an CRITICAL status and appropriate message
-func Critical(err error) {
-	ExitWithStatus(&NagiosStatus{err.Error(), NAGIOS_CRITICAL})
-}
-
-// Exit with an WARNING status and appropriate message
-func Warning(output string) {
-	ExitWithStatus(&NagiosStatus{output, NAGIOS_WARNING})
-}
-
-// Exit with an OK status and appropriate message
-func Ok(output string) {
-	ExitWithStatus(&NagiosStatus{output, NAGIOS_OK})
-}
-
-// Exit with a particular NagiosStatus
-func ExitWithStatus(status *NagiosStatus) {
-	fmt.Fprintln(os.Stdout, valMessages[status.Value], status.Message)
-	os.Exit(int(status.Value))
-}
 
 // main expects 1 - 3 flags: -host <somehost.com> [-protocol http|https] [-port 80|443|xxx]
 func main() {
@@ -103,19 +47,19 @@ func main() {
 	_, err = resp.Body.Read(oneByte)
 
 	if err != nil {
-		Critical(err)
+		messages.Critical(err)
 	} else {
 		if resp.StatusCode == 503 {
 			msg := *hostPtr + " " + resp.Proto + " " + resp.Status
-			Warning(msg)
+			messages.Warning(msg)
 		}
 		if resp.StatusCode != 200 {
 			msg := *hostPtr + " " + resp.Proto + " " + resp.Status
 			err = errors.New(msg)
-			Critical(err)
+			messages.Critical(err)
 		} else {
 			msg := *hostPtr + " responded with " + resp.Proto + " " + resp.Status + " with TTFB of " + strconv.FormatFloat(time.Since(start).Seconds(), 'f', 6, 64) + "s"
-			Ok(msg)
+			messages.Ok(msg)
 		}
 	}
 
---
+++ messages/messages.go
@@ -0,0 +1,69 @@
+/*
+Generate messages to send to Nagios
+*/
+package messages
+
+import (
+	"fmt"
+	"os"
+)
+
+type NagiosStatusVal int
+
+const (
+	NAGIOS_OK       NagiosStatusVal = iota
+	NAGIOS_WARNING
+	NAGIOS_CRITICAL
+	NAGIOS_UNKNOWN
+)
+
+var (
+	valMessages = []string{
+		"OK:",
+		"WARNING:",
+		"CRITICAL:",
+		"UNKNOWN:",
+	}
+)
+
+// Find the highest value and combine all the messages. Things win in the order of highest to lowest.
+type NagiosStatus struct {
+	Message string
+	Value   NagiosStatusVal
+}
+
+func (status *NagiosStatus) Aggregate(otherStatuses []*NagiosStatus) {
+	for _, s := range otherStatuses {
+		if status.Value < s.Value {
+			status.Value = s.Value
+		}
+
+		status.Message += " - " + s.Message
+	}
+}
+
+// Exit with an UNKNOWN status and appropriate message
+func Unknown(output string) {
+	ExitWithStatus(&NagiosStatus{output, NAGIOS_UNKNOWN})
+}
+
+// Exit with an CRITICAL status and appropriate message
+func Critical(err error) {
+	ExitWithStatus(&NagiosStatus{err.Error(), NAGIOS_CRITICAL})
+}
+
+// Exit with an WARNING status and appropriate message
+func Warning(output string) {
+	ExitWithStatus(&NagiosStatus{output, NAGIOS_WARNING})
+}
+
+// Exit with an OK status and appropriate message
+func Ok(output string) {
+	ExitWithStatus(&NagiosStatus{output, NAGIOS_OK})
+}
+
+// Exit with a particular NagiosStatus
+func ExitWithStatus(status *NagiosStatus) {
+	fmt.Fprintln(os.Stdout, valMessages[status.Value], status.Message)
+	os.Exit(int(status.Value))
+}
